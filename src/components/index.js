8eb4cd8d3628e4eaad1f034dec964f490258d067
kenseok <kenseok@gmail.com>
2020-11-26 02:46:42 +0900

    covid-19

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+// 모든 구성 요소 카드 차트를 가져 와서 선택기를 계산
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';
